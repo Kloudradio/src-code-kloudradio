7ed905d39c6b7f5db42c16512222bdb4d819bce6
DanielRosatto <danielrosatto@gmail.com>
2022-03-10 18:47:13 -0300

    [set-code-to-heroku]: Se agrega comentario

--- set-code-to-heroku.sh
+++ set-code-to-heroku.sh
@@ -1,10 +1,12 @@
 cd kloudradio
-cat .env | sed 's/RAILS_ENV=development/RAILS_ENV=production/g' > .env
-cat .env | sed 's/NODE_ENV=development/NODE_ENV=production/g' > .env
+cat .env | sed -i -e 's/RAILS_ENV=development/RAILS_ENV=production/g' -e 's/NODE_ENV=development/NODE_ENV=production/g' .env
 heroku login
 heroku container:login
 docker-compose up --build -d
 docker-compose exec rails db:migrate
+git add . 
+git commit -m "[set-code-to-heroku]: $1"
+git push heroku main
 heroku container:push web
 heroku container:release web
 cd ..
