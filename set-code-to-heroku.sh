7107b720541c5a2a1c83114c3f2029ecd2c36191
DanielRosatto <danielrosatto@gmail.com>
2022-03-10 19:19:15 -0300

    [set-code-to-heroku]: Segunda oportunidad de que se agrega comentario de lema

--- set-code-to-heroku.sh
+++ set-code-to-heroku.sh
@@ -3,7 +3,7 @@ cat .env | sed -i -e 's/RAILS_ENV=development/RAILS_ENV=production/g' -e 's/NODE
 heroku login
 heroku container:login
 docker-compose up --build -d
-docker-compose exec rails db:migrate
+docker-compose exec rails db:setup
 git add . 
 git commit -m "[set-code-to-heroku]: $1"
 git push heroku main
