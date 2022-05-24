4d99d3f38c396136d60828b87235bf88667cd28e
DanielRosatto <danielrosatto@gmail.com>
2022-05-24 11:52:18 -0300

    [set-code-to-heroku]: Se arregla assets...

--- set-code-to-heroku.sh
+++ set-code-to-heroku.sh
@@ -4,11 +4,11 @@ cat .env | sed -i -e 's/RAILS_ENV=development/RAILS_ENV=production/g' -e 's/NODE
 cat .env | sed -i -e 's/export KLOUDRADIO_HOST=localhost/export KLOUDRADIO_HOST=kloudradio.herokuapp.com/g'  .env
 heroku login
 heroku container:login
-docker-compose up --build -d
-docker-compose exec rails db:create
-docker-compose exec rails db:migrate
-git add . 
-git commit -m "[set-code-to-heroku]: $1"
+docker-compose up --build -d &&
+docker-compose exec rails db:create &&
+docker-compose exec rails db:migrate &&
+git add . &&
+git commit -m "[set-code-to-heroku]: $1" &&
 git push heroku main
 heroku container:push web
 heroku container:release web
