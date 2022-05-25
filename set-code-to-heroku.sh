c280005d5437fbc268658665a62fd9f81770ee24
DanielRosatto <danielrosatto@gmail.com>
2022-05-25 13:42:42 -0300

    [set-code-to-heroku]: loop video show

--- set-code-to-heroku.sh
+++ set-code-to-heroku.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd kloudradio
-# cat .env | sed -i -e 's/RAILS_ENV=development/RAILS_ENV=production/g' -e 's/NODE_ENV=development/NODE_ENV=production/g' .env
-# cat .env | sed -i -e 's/export KLOUDRADIO_HOST=localhost/export KLOUDRADIO_HOST=kloudradio.herokuapp.com/g'  .env
+cat .env | sed -i -e 's/RAILS_ENV=development/RAILS_ENV=production/g' -e 's/NODE_ENV=development/NODE_ENV=production/g' .env
+cat .env | sed -i -e 's/export KLOUDRADIO_HOST=localhost/export KLOUDRADIO_HOST=kloudradio.herokuapp.com/g'  .env
 heroku login
 heroku container:login
 docker-compose up --build -d &&
@@ -12,6 +12,8 @@ git commit -m "[set-code-to-heroku]: $1" &&
 git push heroku main
 heroku container:push web
 heroku container:release web
+heroku run rake db:create
+heroku run rake db:migrate
 cd ..
 # tar --exclude={".git",".github"} -cf - kloudradio | (cd __last_code__ && tar -xf -)
 git add . 
