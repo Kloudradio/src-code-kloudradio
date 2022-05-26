5aec9437b62cba6f7ec2ffa6dfded48f24a60031
DanielRosatto <danielrosatto@gmail.com>
2022-05-25 22:05:38 -0300

    [Owner:          kriminal.radio.17.10@gmail.com]: Se mejora link de video en show

--- set-code-to-heroku.sh
+++ set-code-to-heroku.sh
@@ -14,15 +14,16 @@ git commit -m "[Kloudradio committer]: $1" &&
 git push heroku main
 heroku container:push web
 heroku container:release web
-heroku config:set SECRET_KEY_BASE=$(docker-compose exec rails secret)
+heroku config:set SECRET_KEY_BASE=$AUXSECRET
 heroku run rake db:create
 heroku run rake db:migrate
-cd ..
-# tar --exclude={".git",".github"} -cf - kloudradio | (cd __last_code__ && tar -xf -)
+echo "Secret Baase es $AUXSECRET"
 cat .env | sed -i -e 's/RAILS_ENV=production/RAILS_ENV=development/g' -e 's/NODE_ENV=production/NODE_ENV=development/g' .env
 cat .env | sed -i -e 's/export KLOUDRADIO_HOST=kloudradio.herokuapp.com/export KLOUDRADIO_HOST=localhost/g'  .env
 cat .env | sed -i -e "s#export SECRET_KEY_BASE=$AUXSECRET#export SECRET_KEY_BASE=insecure_key_for_dev#" .env
 AUXSECRET=$( heroku info | grep Owner )
+cd ..
+# tar --exclude={".git",".github"} -cf - kloudradio | (cd __last_code__ && tar -xf -)
 git add . 
 git commit -m "[$AUXSECRET]: $1"
 git push
